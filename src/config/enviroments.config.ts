c599a8955895cfe63dd0e1d4d1ba6e806b8f8110
Alexander Sauro <112291051+alxbro1@users.noreply.github.com>
2024-10-18 15:39:55 -0300

    fix(main.ts) 'now the listen a port that is specific in .env (PORT)'

--- src/config/enviroments.config.ts
+++ src/config/enviroments.config.ts
@@ -3,3 +3,4 @@ import {config} from 'dotenv'
 config({path:'.env'});
 
 export const POSTGRES_URL = process.env.POSTGRES_URL;
+export const PORT = process.env.PORT;
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 // import { BadRequestException, ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { PORT } from './config/enviroments.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -54,6 +55,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, swaggerConfig);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3001);
+  await app.listen(PORT || 3001);
 }
 bootstrap();
